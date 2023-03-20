6fbdb13e26f55a40519b70ab782adb8ac8557ae2
sharon <sharonraletswetsi@gmail.com>
2023-03-20 08:28:04 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf("%d is negetivr\n", n);
+		printf("%d is negetive\n", n);
 	}
 	else
 	{
-		printf("%d is negetive\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }
